e6df86c342a69cd50f872d80c3d1bb0e77cd2612
Akhil <akhiltiwari.dev@gmail.com>
2026-08-15 16:00:06 +0530

    Update dependencies to include Octokit packages in bun.lock and package.json; modify Next.js configuration to allow specific development origins; enhance auth-proxy.ts for session management and improve Prisma schema with Github installation model.

---
+++ features/github/utils/github-app.ts
@@ -0,0 +1,38 @@
+//  ocktokit package used here
+
+import {App} from "octokit"
+
+let githubApp: App | null= null;
+
+const appId = process.env.GITHUB_APP_ID!;
+const privateKey = process.env.GITHUB_APP_PRIVATE_KEY?.replace(/\\n/g,"\n")!;
+
+
+// if (!appId || !privateKey) {
+//   throw new Error("Missing GITHUB_APP_ID or GITHUB_PRIVATE_KEY in environment variables");
+// }
+
+export function getGithubApp(){
+    if(!githubApp){
+        githubApp= new App({
+            appId : appId,
+            privateKey: privateKey,
+            webhooks:{
+                secret:process.env.GITHUB_WEBHOOK_SECRET!
+            }
+            
+        })
+    }
+    return githubApp;
+}
+
+
+// github app installation url
+
+
+export function getGithubInstallUrl(userId: string){
+    const appName= process.env.GITHUB_APP_NAME!;
+    const url= new URL(`https://github.com/apps/${appName}/installations/new`);
+    url.searchParams.set("state",userId);
+    return url.toString();
+}
